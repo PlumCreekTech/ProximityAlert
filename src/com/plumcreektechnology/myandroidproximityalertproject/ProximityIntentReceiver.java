6ab6daffc0f2d6db4f959489b84a6ad91407da0d
Devin Frenze <frenze.devin@gmail.com>
2013-07-03 11:23:18 -0400

    runs on API 16

--- src/com/plumcreektechnology/myandroidproximityalertproject/ProximityIntentReceiver.java
+++ src/com/plumcreektechnology/myandroidproximityalertproject/ProximityIntentReceiver.java
@@ -2,11 +2,13 @@ package com.plumcreektechnology.myandroidproximityalertproject;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -26,16 +28,24 @@ public class ProximityIntentReceiver extends BroadcastReceiver {
 			Log.d(getClass().getSimpleName(), "exiting");
 		}
 		
+		// create a pending intent to be activated from notificaiton
+		Uri uri = Uri.parse("https://twitter.com/StealthMountain");
+		Intent pending = new Intent(Intent.ACTION_VIEW, uri);
+		pending.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		//context.startActivity(pending);
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, pending, 0);
+		
 		// create a notification of the proximity alert
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
 				.setSmallIcon(R.drawable.ic_launcher)
 				.setContentTitle("We did it!")
-				.setContentText("You're near the POI, sort of.");
+				.setContentText("You're near the POI, sort of.")
+				.addAction(R.drawable.ic_launcher, "sneak peek of what's to come", pendingIntent);
 		
 		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.notify(17, builder.build());
+		notificationManager.notify(NOTIFICATION_ID, builder.build());
 		
-		//PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, null, 0);
+		
 		
 //		Notification notification = new Notification.Builder(context)
 //				.setContentTitle("Proximity Alert!")
